35d15d419be89f5518fae5a3d35549521f1ec794
pauloPedrob <siqueirapedropaulo93@gmail.com>
2023-10-07 17:40:19 -0300

    att

--- metodosageis.py
+++ metodosageis.py
@@ -49,7 +49,7 @@ def validar_telefone(telefone):
     telefone = re.sub(r'[^\d]', '', telefone)
 
     # Verifica se o telefone possui pelo menos 8 dígitos (DDD + número local)
-    if len(telefone) < 8:
+    if len(telefone) < 8 or len(telefone) > 11:
         return False
 
     # Verifica se os primeiros 2 dígitos correspondem a um DDD válido (códigos reais podem variar)
@@ -90,140 +90,164 @@ def verificar_senha(senha):
     return True
 
 while(acabar == False):
-    print("BEM VINDO AO DEFINIR NOME\n")
+    try:
+        print("BEM VINDO AO DEFINIR NOME\n")
 
-    opition = int(input("QUAL OPÇÃO VOCÊ DESEJA?\n1- LOGIN\n2- CADASTRO\n3-RECUPERAR SENHA\n"))
+        opition = int(input("QUAL OPÇÃO VOCÊ DESEJA?\n1- LOGIN\n2- CADASTRO\n3-RECUPERAR SENHA\n"))
 
-    if opition == 1:
-        email = input("DIGITE SEU EMAIL:\n")
-        senha = input("DIGITE SUA SENHA:\n")
+        if opition == 1:
+            email = input("DIGITE SEU EMAIL:\n")
+            senha = input("DIGITE SUA SENHA:\n")
 
-        cursor.execute("SELECT senha_usuario FROM tbl_usuario WHERE email_usuario=%s", (email,))
-      
-        registro = cursor.fetchone()
-
-        if registro:
-            senha_armazenada = registro[0]
-            
-            if bcrypt.checkpw(senha.encode('utf-8'), senha_armazenada.encode('utf-8')):
-                print("Login bem-sucedido.")
-            else:
-                print("Senha incorreta.")
-        else:
-            print('Email incorreto.')
-
-
+            cursor.execute("SELECT * FROM tbl_usuario WHERE email_usuario=%s", (email,))
         
+            registro = cursor.fetchone()
 
-    elif opition == 2:
-        emailcerto=False
-        datacerta = False
-        senhasiguais = False
-        nomevalido = False
-        cpfvalido = False
-        telvalido = False
-
-        while(emailcerto ==False):
-            email = input("DIGITE O EMAIL:\n")
-            regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-            if re.match(regex, email):
-                emailcerto= True
-            else:
-                print("Email inválido")
+            if registro:
+                senha_armazenada = registro[6]
                 
-        while(senhasiguais ==False):
-            senha = input("DIGITE A SENHA:\n")
-            confsenha = input("DIGITE A CONFIRMAÇÃO DE SENHA SENHA:\n")
-
-            if(senha == confsenha):
-                if verificar_senha(senha):
-                    senhasiguais = True
+                if bcrypt.checkpw(senha.encode('utf-8'), senha_armazenada.encode('utf-8')):
+                    print(f"\nLOGIN BEM SUCEDIDO\n---------------\n")
+                    menu = True
+                    while(menu == True):
+                        print(f"BEM-VINDO(A) {registro[1]}\nVocê tem R$ {registro[7]}\n")
+                        try:
+                            opition_menu = int(input("Qual opção você deseja?\n 1- RENDAS E DESPESAS\n 2- CALENDÁRIO\n"))
+                        except ValueError:
+                            print("ALGO DEU ERRADO\n")
                 else:
-                    print("Senha deve conter: no mínimo oito caracteres, um caracter especial, um número e uma letra maiúscula")
+                    print("\n SENHA INCORRETA\n")
             else:
-                print("Senhas não conferem")
-
-        while(nomevalido==False):
-            regex = r'^[A-Za-z\s]{2,50}$'
-            nome = input("DIGITE O NOME")
-            if re.match(regex, nome):
-                nomevalido= True
-            else:
-                print("Nome inválido")
-
-        while (datacerta == False):
-            dt = input("Digite sua data de nascimento. (dd/mm/ano)")
-            try:
-                data = datetime.strptime(dt, '%d/%m/%Y')
-                print("Data inserida:", data)
-                datacerta = True
-            except ValueError:
-                print("Formato de data inválido. Certifique-se de usar o formato 'dd/mm/aaaa'.")
-        
-        while(cpfvalido == False):
-            cpf = input("Digite o cpf")
-            if validar_cpf(cpf):
-                cpfvalido = True
-            else:
-                print("CPF INVÁLIDO")
-                
-        while(telvalido ==  False):
-            tel = input("Telefone do usuário (COM DD)")
-            if validar_telefone(tel):
-                telvalido = True
-            else:
-                print("Telefone inválido")
-
-
-      
-        salt = bcrypt.gensalt(12)
-        hashed_senha = bcrypt.hashpw(senha.encode('utf-8'), salt)
-
-        comand = 'INSERT INTO tbl_usuario (email_usuario, senha_usuario,cpf_usuario,telefone_usuario,data_nasc_usuario,nome_usuario,saldo_atual_usuario,status_usuario) VALUES (%s, %s,%s,%s,%s,%s,0,true)'
-        valores = (email, hashed_senha,cpf,tel,data,nome)
-        cursor.execute(comand, valores)
-        conexao.commit()
+                print('\nEMAIL INCORRETO\n')
 
 
             
-    elif opition == 3:
-        email = input("DIGITE O EMAIL:\n")
-       
-        cursor.execute("SELECT senha_usuario FROM tbl_usuario WHERE email_usuario=%s", (email,))
-        registro = cursor.fetchone()
 
-        if(registro):
-            letras = ''.join(random.choice(string.ascii_lowercase) for _ in range(3))
-            numeros = ''.join(random.choice(string.digits) for _ in range(3))
-            codigo = letras + numeros
-            print(f"Seu código é:\n---------\n{codigo}\n---------\n")
-            cod = input("DIGITE O CÓDIGO:\n")
-            if cod == codigo:
-                
-                 senhasiguais = False
-                 while(senhasiguais == False):
-                    novasenha = input("Digite a nova senha:")
-                    confnovasenha = input("confirme a nova senha")
-                    if(novasenha == confnovasenha):
-                        salt = bcrypt.gensalt(12)
-                        hashed_novasenha = bcrypt.hashpw(novasenha.encode('utf-8'), salt)
-                        if verificar_senha(novasenha):
-                            comand = 'UPDATE tbl_usuario SET senha_usuario = %s WHERE email_usuario = %s'
-                            valores = (hashed_novasenha,email)
-                            cursor.execute(comand,valores)
-                            conexao.commit()
-                            senhasiguais = True
-                        else:
-                            print("Senha deve conter: no mínimo oito caracteres, um caracter especial, um número e uma letra maiúscula")
+        elif opition == 2:
+            emailcerto=False
+            datacerta = False
+            senhasiguais = False
+            nomevalido = False
+            cpfvalido = False
+            telvalido = False
+            saldocorreto = False
+
+            while(emailcerto ==False):
+                email = input("DIGITE O EMAIL:\n")
+                regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+                if re.match(regex, email):
+                    cursor.execute("SELECT * FROM tbl_usuario WHERE email_usuario=%s", (email,))
+                    registro = cursor.fetchone()
+                    if registro:
+                        print("Email já registrado na nossa base de dados")
+                    else: 
+                        emailcerto= True
+                else:
+                    print("Email inválido")
+                    
+            while(senhasiguais ==False):
+                senha = input("DIGITE A SENHA:\n")
+                confsenha = input("DIGITE A CONFIRMAÇÃO DE SENHA SENHA:\n")
+
+                if(senha == confsenha):
+                    if verificar_senha(senha):
+                        senhasiguais = True
                     else:
-                        print("Senhas não conferem!")
+                        print("Senha deve conter: no mínimo oito caracteres, um caracter especial, um número e uma letra maiúscula")
+                else:
+                    print("Senhas não conferem")
 
+            while(nomevalido==False):
+                regex = r'^[A-Za-z\s]{2,50}$'
+                nome = input("DIGITE O NOME")
+                if re.match(regex, nome):
+                    nomevalido= True
+                else:
+                    print("Nome inválido")
+
+            while (datacerta == False):
+                dt = input("Digite sua data de nascimento. (dd/mm/ano)")
+                try:
+                    data = datetime.strptime(dt, '%d/%m/%Y')
+                    print("Data inserida:", data)
+                    datacerta = True
+                except ValueError:
+                    print("Formato de data inválido. Certifique-se de usar o formato 'dd/mm/aaaa'.")
             
-            
+            while(cpfvalido == False):
+                cpf = input("Digite o cpf")
+                if validar_cpf(cpf):
+                    cpfvalido = True
+                else:
+                    print("CPF INVÁLIDO")
+                    
+            while(telvalido ==  False):
+                tel = input("Telefone do usuário (COM DD)")
+                if validar_telefone(tel):
+                    telvalido = True
+                else:
+                    print("Telefone inválido")
+            while(saldocorreto == False):
+                try:
+                    saldo = float(input("Saldo atual: R$ "))
+                    if(saldo >= 0):
+                        saldocorreto = True
+                except ValueError:
+                    print("INVÁLIDO\n")
 
-        else:
-            print("Email não encontrado")
+        
+            salt = bcrypt.gensalt(12)
+            hashed_senha = bcrypt.hashpw(senha.encode('utf-8'), salt)
 
+            comand = 'INSERT INTO tbl_usuario (email_usuario, senha_usuario,cpf_usuario,telefone_usuario,data_nasc_usuario,nome_usuario,saldo_atual_usuario,status_usuario) VALUES (%s, %s,%s,%s,%s,%s,%s,true)'
+            valores = (email, hashed_senha,cpf,tel,data,nome,saldo)
+            cursor.execute(comand, valores)
+            conexao.commit()
+
+
+                
+        elif opition == 3:
+            email = input("DIGITE O EMAIL:\n")
+        
+            cursor.execute("SELECT senha_usuario FROM tbl_usuario WHERE email_usuario=%s", (email,))
+            registro = cursor.fetchone()
+
+            if(registro):
+                letras = ''.join(random.choice(string.ascii_lowercase) for _ in range(3))
+                numeros = ''.join(random.choice(string.digits) for _ in range(3))
+                codigo = letras + numeros
+                print(f"Seu código é:\n---------\n{codigo}\n---------\n")
+                cod = input("DIGITE O CÓDIGO:\n")
+                if cod == codigo:
+                    
+                    senhasiguais = False
+                    while(senhasiguais == False):
+                        novasenha = input("Digite a nova senha:")
+                        confnovasenha = input("confirme a nova senha")
+                        if(novasenha == confnovasenha):
+                            salt = bcrypt.gensalt(12)
+                            hashed_novasenha = bcrypt.hashpw(novasenha.encode('utf-8'), salt)
+                            if verificar_senha(novasenha):
+                                comand = 'UPDATE tbl_usuario SET senha_usuario = %s WHERE email_usuario = %s'
+                                valores = (hashed_novasenha,email)
+                                cursor.execute(comand,valores)
+                                conexao.commit()
+                                senhasiguais = True
+                            else:
+                                print("Senha deve conter: no mínimo oito caracteres, um caracter especial, um número e uma letra maiúscula")
+                        else:
+                            print("Senhas não conferem!")
+
+                
+                
+
+            else:
+                print("Email não encontrado")
+
+    except ValueError:
+
+        print("\n ALGO DEU ERRADO\n")
+    
 
    
 
